72073511107877892b11def93e06100bca941873
Gavin Davenport <gavinkdavenport@gmail.com>
2024-03-05 22:06:42 -0800

    Create factory function for menu items
    factory function and adjusting the loop to be based off of an array of menu item objects allows for easier menu editing later

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,18 @@
+let menuList = [];
+
+function menuDish(imgLnk, imgAlt, title, desc) {
+    const imgLnk = imgLnk;
+    const imgAlt = imgAlt;
+    const title = title;
+    const desc = desc;
+    return { imgLnk, imgAlt, title, desc };
+};
+
+menuList.push(menuDish('', 'A plain cheese pizza', "Cheese Pizza", "A cheese pizza with your choice of up to four cheeses!"));
+menuList.push(menuDish('', 'A pepperoni pizza', "One-topping pizza", "A single-topping pizza with your choice of our many topping options!"));
+menuList.push(menuDish('', 'A Hawaiian-style pizza', "Two-topping pizza", "Our classic choice, choose two toppings of any kind!"));
+menuList.push(menuDish('', "A toasted s'more by a campfire", "Dessert S'more", "A hand-roasted s'more cooked to your liking and with your choice of chocolate!"));
+
 function goMenu() {
     const content = document.querySelector('#content');
 
@@ -5,36 +20,25 @@ function goMenu() {
     title.innerHTML = "Menu";
     content.appendChild(title);
 
-    for (let i = 0; i < 3; i++) {
+    for (let i = 0; i < menuList.length; i++) {
         const menuItem = document.createElement('div');
         menuItem.classList.add('menu-item');
         content.appendChild(menuItem);
 
         const menuImage = document.createElement('img');
         menuImage.classList.add('menu-image');
+        menuImage.src = menuList[i].imgLnk;
+        menuImage.alt = menuList[i].imgAlt;
         menuItem.appendChild(menuImage);
 
         const itemTitle = document.createElement('h2');
-        menuItem.classList.add('item-title');
+        itemTitle.classList.add('item-title');
+        itemTitle.innerHTML = menuList[i].title;
         menuItem.appendChild(itemTitle);
 
         const itemDesc = document.createElement('p');
-        menuItem.classList.add('item-description');
+        itemDesc.classList.add('item-description');
+        itemDesc.innerHTML = menuList[i].desc;
         menuItem.appendChild(itemDesc);
     };
-
-    menuImage[0].src = '';// image link
-    menuImage[0].alt = 'A plain cheese pizza';
-    itemTitle[0].innerHTML = "Cheese Pizza";
-    itemDesc[0].innerHTML = "A cheese pizza with your choice of up to four cheeses!";
-
-    menuImage[1].src = '';// image link
-    menuImage[1].alt = 'A pepperoni pizza';
-    itemTitle[1].innerHTML = "One-topping pizza";
-    itemDesc[1].innerHTML = "A single-topping pizza with your choice of our many topping options!";
-
-    menuImage[2].src = '';// image link
-    menuImage[2].alt = 'A Hawaiian-style pizza';
-    itemTitle[2].innerHTML = "Two-topping pizza";
-    itemDesc[2].innerHTML = "Our classic choice, choose two toppings of any kind!";
 };
