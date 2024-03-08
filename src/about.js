9c09492672d439f0ce7deb41cb15d3819d0d1f2e
Gavin Davenport <gavinkdavenport@gmail.com>
2024-03-08 15:04:30 -0800

    Build about page generation and page clear
    functions for additional page functionality

---
+++ src/about.js
@@ -0,0 +1,16 @@
+function goAbout() {
+    const content = document.querySelector('#content');
+    
+    const title = document.createElement('h1');
+    title.innerHTML = "Born in the outdoors.";
+    content.appendChild(title);
+
+    const pizzaImg = document.createElement('img');
+    aboutIMG.src = '';
+    pizzaImg.alt = "A campfire in the woods";
+    content.appendChild(aboutImg);
+
+    const description = document.createElement('p');
+    description.innerHTML = "The idea for this was conceived right where you'd want it to be - over a campfire one night in the woods, with a few friends, a few drinks, and some unexpectedly good food.  From there it sat dormant for a while as more expected career options were pursued, but eventually, the woods came calling loud enough that we figured we'd give this a shot, now here we are!";
+    content.appendChild(description);
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import './home.js';
 import './menu.js';
+import './about.js'
 
 console.log("Webpack running");
 
 goHome();
+
+function clear() {
+    const content = document.querySelector('#content');
+    while (content.firstChild) {
+        content.removeChild(container.firstChild);
+    };
+};
